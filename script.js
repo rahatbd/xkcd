4e6127b36f5ab18ec5026d21cb7b2b7c755b4fc6
Rahat Rahman <63892310+rahatbd@users.noreply.github.com>
2021-07-14 00:16:39 -0400

    add event handlers

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const comic = document.querySelector('.comic');
 const buttons = document.querySelectorAll('button');
+const form = document.querySelector('form');
+const input = document.querySelector('input[type="search"]');
 
 // const api = 'http://xkcd.com/info.0.json';
 // const proxy = 'https://secure-springs-24728.herokuapp.com/';
@@ -10,6 +12,7 @@ let maxNum;
 
 async function fetchComic(num = '') {
     if (num === 404) {
+        currentNum = 404;
         comic.innerHTML = `
             <div>
                 <img src="https://www.explainxkcd.com/wiki/images/9/92/not_found.png" alt="404 Not Found"/>
@@ -21,6 +24,9 @@ async function fetchComic(num = '') {
     const data = await response.json();
     currentNum = data.num;
     if (num === '') maxNum = currentNum;
+    form.search.setAttribute('placeholder', `# 1 \u2014 ${maxNum}`);
+    // form.search.setAttribute('pattern', `/^[1-9][0-9]?$|^${maxNum}$/`);
+    form.search.setAttribute('title', `# 1 -- ${maxNum}`);
     comic.innerHTML = `
         <div>
             <h2>#${data.num}: ${data.title}</h2>
@@ -30,21 +36,39 @@ async function fetchComic(num = '') {
             <img src="${data.img}" alt="${data.alt}"/>
         </div>
     `;
+    // transcript
     console.log(data);
+    const title = document.querySelector('title');
+    title.textContent = `xkcd: ${data.title}`;
 }
 
-buttons.forEach(button => button.addEventListener('click', function() {
-    if (this.id === 'prev') {
+function handleEvent(event) {
+    if (event.keyCode === 32 || event.keyCode === 37 || event.keyCode === 39) event.preventDefault();
+    if (this.id === 'prev' || event.keyCode === 37) {
         currentNum === 1 ? fetchComic() : fetchComic(currentNum - 1);
     }
-    else if (this.id === 'random') {
+    else if (this.id === 'random' || event.keyCode === 32) {
         const randomNum = Math.floor(Math.random() * maxNum) + 1;
         fetchComic(randomNum);
     }
-    else if (this.id === 'next') {
+    else if (this.id === 'next' || event.keyCode === 39) {
         currentNum === maxNum ? fetchComic(1) : fetchComic(currentNum + 1);
     }
-}))
+}
+
+function handleSubmit(event) {
+    event.preventDefault();
+    fetchComic(parseInt(form.search.value));
+    form.reset();
+}
+
+// input.addEventListener('focus', function() {
+//     form.search.setAttribute('value', '# ');
+// })
+
+buttons.forEach(button => button.addEventListener('click', handleEvent));
+window.addEventListener('keydown', handleEvent);
+form.addEventListener('submit', handleSubmit);
 
 // DOMContentLoaded 1945
 fetchComic();
