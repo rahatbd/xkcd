58a9451b45a4ba58edf5b4d644c603f454ad7e78
Rahat Rahman <63892310+rahatbd@users.noreply.github.com>
2022-12-01 17:34:17 -0500

    update proxy server

--- script.js
+++ script.js
@@ -29,7 +29,8 @@ async function fetchComic(num = '') {
     section.classList.add('hidden');
     if (currentNum === '') theme.classList.add('hidden');
     window.removeEventListener('keydown', handleEvent);
-    const response = await fetch(`https://secure-springs-24728.herokuapp.com/http://xkcd.com/${currentNum}/info.0.json`);
+    // const response = await fetch(`https://secure-springs-24728.herokuapp.com/http://xkcd.com/${currentNum}/info.0.json`);
+    const response = await fetch(`https://proxy-ugwolsldnq-uc.a.run.app/https://xkcd.com/${currentNum}/info.0.json`);
     const data = await response.json();
     if (currentNum === '') {
         currentNum = data.num;
